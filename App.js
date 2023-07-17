441a15a9886f2fe62c9d089a8376b78dcfa625dc
Parul Tater <parul@Paruls-MacBook-Air.local>
2023-07-17 12:29:51 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", {}, "Hello World!")
+
+const parent = React.createElement("div", {id: "parent"}, 
+[React.createElement("div", {id: "child1"}, 
+    [React.createElement("h1", {}, "I am a h1 element"),
+    React.createElement("h2", {}, "I am a h2 element")]
+    )],
+[React.createElement("div", {id: "child2"}, 
+    [React.createElement("h1", {}, "I am a h1 element"),
+    React.createElement("h2", {}, "I am a h2 element")]
+)])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
